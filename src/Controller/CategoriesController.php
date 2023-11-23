afa1d777578c80fa50bbeedf5c5c05a3a4b2d234
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-23 17:47:13 +0100

    Add form handling logic to CategoriesController

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use App\Form\CategorieType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;   
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,7 +21,7 @@ class CategoriesController extends AbstractController
     }
     //action pour ajouter une categorie
     #[Route('/categories/ajouter', name: 'app_categories_ajouter')]
-    public function ajouter(): Response
+    public function ajouter(Request $request, ManagerRegistry $managerRegistry): Response
     {
         //création d'un formulaire
         //on créé un objet de la classe Categorie
@@ -28,8 +30,17 @@ class CategoriesController extends AbstractController
         $form = $this->createForm(CategorieType::class, $categorie);
 
         //todo : traiter le formulaire en retour
-
-        
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //on récupère le manager de doctrine
+            $manager = $managerRegistry->getManager();
+            //on demande au manager de sauvegarder l'objet $categorie
+            $manager->persist($categorie);
+            //on demande au manager d'envoyer les requêtes SQL
+            $manager->flush();
+            //on redirige vers l'index des catégories
+            return $this->redirectToRoute('app_categories');
+        }
 
 
         return $this->render('categories/ajouter.html.twig', [
