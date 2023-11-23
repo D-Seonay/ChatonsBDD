d8de65388395dbff71b00af66594a58f5b26513a
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-23 17:30:29 +0100

    Add Categorie entity, CategorieType form, and ajouter.html.twig template

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,8 +21,20 @@ class CategoriesController extends AbstractController
     #[Route('/categories/ajouter', name: 'app_categories_ajouter')]
     public function ajouter(): Response
     {
+        //création d'un formulaire
+        //on créé un objet de la classe Categorie
+        $categorie = new Categorie();
+        //on créé un formulaire en utilisant la méthode createForm()
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        //todo : traiter le formulaire en retour
+
+        
+
+
         return $this->render('categories/ajouter.html.twig', [
             'controller_name' => 'CategoriesController',
+            'formulaire' => $form->createView()
         ]);
     }
 }
